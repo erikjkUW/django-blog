d3a9a0f58899fc9f30a0419682ca51891e32d01e
erikjkUW <erikjk@uw.edu>
2021-05-18 20:03:58 -0700

    Added Global Nav and Login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polling/', include('polling.urls')),
     path('', include('blogging.urls')),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout")
 ]

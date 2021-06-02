4cd7820a0131895cdd084569f35b181eb597589a
erikjkUW <erikjk@uw.edu>
2021-06-02 16:27:39 -0700

    Added github login

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.github",
     "polling",
     "blogging",
 ]
@@ -125,3 +130,12 @@ STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+# allauth backend
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+SITE_ID = 2
+ACCOUNT_EMAIL_VERIFICATION = 'none'

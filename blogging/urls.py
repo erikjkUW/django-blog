55a71d897c5f5c617aea35d9e6d5f5cffd46d9b5
erikjkUW <erikjk@uw.edu>
2021-06-03 09:47:44 -0700

    Completed rest framework api

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from blogging.views import PostListView, PostDetailView
+from django.urls import path, include
+from rest_framework import routers
+from blogging.views import PostListView, PostDetailView, add_model, UserViewSet, PostViewSet, CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
+    path("api/", include(router.urls)),
+    path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
     path("", PostListView.as_view(), name="blog_index"),
+    path("add/", add_model, name="add_post"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
+    "rest_framework",
     "polling",
     "blogging",
 ]
@@ -139,3 +140,8 @@ AUTHENTICATION_BACKENDS = (
 )
 SITE_ID = 2
 ACCOUNT_EMAIL_VERIFICATION = "none"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
